d6ee1175c41fdc30b0436e31c88efb557e66c6ef
Zeadrasil <73188667+Zeadrasil@users.noreply.github.com>
2020-10-26 19:45:34 -0500

    Create Form1.cs

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TD_Game
+{
+    public partial class towerDefense : Form
+    {
+        public towerDefense()
+        {
+            InitializeComponent();
+        }
+
+        public void disableAll()
+        {
+            abilitiesButton.Enabled = false;
+            upgradesButton.Enabled = false;
+            quitButton.Enabled = false;
+            towersButton.Enabled = false;
+            tutorialButton.Enabled = false;
+            enemiesButton.Enabled = false;
+        }
+
+        public void enableAll()
+        {
+            abilitiesButton.Enabled = true;
+            upgradesButton.Enabled = true;
+            quitButton.Enabled = true;
+            towersButton.Enabled = true;
+            tutorialButton.Enabled = true;
+            enemiesButton.Enabled = true;
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
